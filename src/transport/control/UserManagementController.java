2b9094128ada5185a5be091bcc8cb0cd97378aa4
Meridja Abdeouahab <na_meridja@esi.dz>
2025-05-19 18:00:33 +0200

    zmr

--- src/transport/control/UserManagementController.java
+++ src/transport/control/UserManagementController.java
@@ -1,4 +1,41 @@
 package transport.control;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
+
 public class UserManagementController {
-}
+
+    public Button AddUserBTN;
+
+
+
+
+
+    @FXML
+    private void test(){
+        System.out.println("test");
+    }
+
+    @FXML
+    private void addUser(ActionEvent event) throws IOException {
+        try {
+            Parent AddUserRoot = FXMLLoader.load(getClass().getResource("/transport/ui/AddUsers.fxml"));
+            Scene secondScene = new Scene(AddUserRoot);
+
+            // Get the stage from the event
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setScene(secondScene);
+            currentStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
